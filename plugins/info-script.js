ad2e5243b22bd4644f2e65a81aa46caf3726b958
Xianzhe ID <115347633+Xianzheid@users.noreply.github.com>
2022-10-12 19:19:55 +0700

    Add files via upload

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,23 +2,23 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://github.com/Xianzhe2/FeixuanBOT-V1*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+https://github.com/Nurutomo/wabot-aq
 
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
 
-wa.me/6282127487538`
+wa.me/6285326781097`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    mediaUrl: "https://telegra.ph/file/9c44ff3dea63746735732.jpg",
+    mediaType: "IMAGE",
+    description: "https://www.instagram.com/xianzhe_xx", 
+    title: 'Feixuan-BOT | MD',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
